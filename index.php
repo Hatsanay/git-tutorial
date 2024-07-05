79239981f4629877bd1516f031f38a645de0c89b
Hatsanay <92789928+Hatsanay@users.noreply.github.com>
2024-07-05 17:12:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
     echo "<h1>my repository</h1>" 
     echo "<h1>my name is hatsanai</h1>" 
+    echo "<h1>i study at mahanakorn university of thecnology</h1>"
+    echo "<h1>my repository</h1>" 
+    echo "<h1>my name is hatsanai</h1>" 
     echo "<h1>i study at mahanakorn university of thecnology</h1>" 
 ?>
